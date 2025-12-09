c4772f3b228ecbd2af9856619426249e100e4099
Alex <gkristoffersen83@hotmail.com>
2025-12-09 18:34:44 +0100

    Revert "Add ffmpeg example (#18)"
    This reverts commit f005e9272ccd01094c060aa7bcb25225de55115e.

--- ffmpeg-processing/src/workflows/audio-convert/steps/index.ts
+++
@@ -1,5 +0,0 @@
-export { createSandbox } from "./create-sandbox";
-export { setupFfmpeg } from "./setup-ffmpeg";
-export { transcode } from "./transcode";
-export { streamOutput } from "./stream-output";
-export { stopSandbox } from "./stop-sandbox";

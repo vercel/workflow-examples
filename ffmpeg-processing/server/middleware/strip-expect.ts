b35538132b9b63ef8dad21c8246043d1b0c205aa
Garrett Tolbert <garrett.tolbert@vercel.com>
2025-12-08 19:07:11 -0600

    remove middleware

--- ffmpeg-processing/server/middleware/strip-expect.ts
+++
@@ -1,7 +0,0 @@
-import { defineEventHandler } from "nitro/h3";
-
-export default defineEventHandler((event) => {
-	if (event.req.headers.get("expect")) {
-		event.req.headers.delete("expect");
-	}
-});
